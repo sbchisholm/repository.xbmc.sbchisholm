02984b53919505b6a3cbf2fd7d768f48751924c3
Stephen B Chisholm <sbchisholm@gmail.com>
2012-12-20 11:43:07 -0400

    refactored the coads

--- plugin.audio.cbc-radio/default.py
+++ plugin.audio.cbc-radio/default.py
@@ -24,13 +24,13 @@ from xml.etree.ElementTree import ElementTree
 
 __XBMC_Revision__ = xbmc.getInfoLabel('System.BuildVersion')
 __settings__      = xbmcaddon.Addon(id='plugin.audio.cbc-radio')
-__home__ = __settings__.getAddonInfo('path')
+__home__          = __settings__.getAddonInfo('path')
 __language__      = __settings__.getLocalizedString
 __version__       = __settings__.getAddonInfo('version')
 __cwd__           = __settings__.getAddonInfo('path')
-__addonname__     = "CBC Radio"
-__addonid__       = "plugin.audio.cbc-radio"
-__author__        = "Stephen B Chisholm"
+__addonname__     = 'CBC Radio'
+__addonid__       = 'plugin.audio.cbc-radio'
+__author__        = 'Stephen B Chisholm'
 
 def url_query_to_dict(url):
   ''' Returns the URL query args parsed into a dictionary '''
@@ -42,7 +42,30 @@ def url_query_to_dict(url):
       param[kvp[0]] = kvp[1]
   return param
 
+def build_station_listing(station):
+  ''' Build the list of station for a category '''
+  for station in stations:
+    u = sys.argv[0] + '?' + urllib.urlencode({'stream':station['url']})
+    liz = xbmcgui.ListItem(station['name'])
+    xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]),
+                                url = u, listitem = liz,
+                                isFolder = False)
+
+def build_category_listing(categories, parent_category):
+  ''' Build the list of categories or subcategories '''
+  for category_item in categories:
+    if parent_category:
+      u = sys.argv[0] + '?category=' + parent_category \
+                      + '&subcategory=' + category_item['name']
+    else:
+      u = sys.argv[0] + '?category=' + category_item['name']
+    liz = xbmcgui.ListItem(category_item['name'])
+    xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]),
+                                url = u, listitem = liz,
+                                isFolder = True)
+
 def main():
+  ''' main entry point where the request are handled '''
   params = url_query_to_dict(sys.argv[2])
   category = params.get('category')
   subcategory = params.get('subcategory')
@@ -50,58 +73,39 @@ def main():
   station_data = json.loads(open(os.path.join(__home__, 'stations.json'), 'r').read())
 
   if stream:
-    print stream
     #play stream
     stream = urllib.url2pathname(stream)
     xbmc.Player().play(stream)
 
   elif category and subcategory:
+    # list all of the station in the subcategory
     category = urllib.unquote(category)
     subcategory = urllib.unquote(subcategory)
-    print 'category:', category, 'subcategory:', subcategory
     for category_item in station_data['categories']:
       if category_item['name'] == category:
         for subcategory_item in category_item['subcategories']:
           if subcategory_item['name'] == subcategory:
-            for station in subcategory_item['stations']:
-              u = sys.argv[0] + '?' + urllib.urlencode({'stream':station['url']})
-              liz = xbmcgui.ListItem(station['name'])
-              xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]),
-                                          url = u, listitem = liz,
-                                          isFolder = False)
+            build_station_listing(subcategory_item['stations'])
+            break
+        break
     xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
   elif category:
-    print 'showing stream from', category
+    # list all the stations or subcategories in the category
     category = urllib.unquote(category)
-    # list all the stations in the category
     for category_item in station_data['categories']:
       if category_item['name'] == category:
         if category_item.has_key('stations'):
-          for station in category_item['stations']:
-            u = sys.argv[0] + '?' + urllib.urlencode({'stream':station['url']})
-            liz = xbmcgui.ListItem(station['name'])
-            xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]),
-                                        url = u, listitem = liz,
-                                        isFolder = False)
+          build_station_listing(category_item['stations'])
         else:
-          for subcategory_item in category_item['subcategories']:
-            u = sys.argv[0] + '?category=' + category + '&subcategory=' + subcategory_item['name']
-            liz = xbmcgui.ListItem(subcategory_item['name'])
-            xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]),
-                                        url = u, listitem = liz,
-                                        isFolder = True)
-
+          build_category_listing(categories = category_item['subcategories'],
+                                 parent_category = category)
+        break
     xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
   else:
     # list all of the categories
-    print 'list all categories'
-    for category in station_data['categories']:
-      u = sys.argv[0] + '?category=' + category['name']
-      liz = xbmcgui.ListItem(category['name'])
-      xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]),
-                                  url = u, listitem = liz,
-                                  isFolder = True)
+    build_category_listing(station_data['categories'])
     xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 # Enter here.
